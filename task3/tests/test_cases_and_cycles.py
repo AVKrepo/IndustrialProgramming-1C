cf360f7b74c175578e044570f026d775ab7d465e
Alexander Kosov <kosov.av@phystech.edu>
2018-04-18 23:49:45 +0300

    Added support for cycles

--- task3/tests/test_cases_and_cycles.py
+++ task3/tests/test_cases_and_cycles.py
@@ -41,3 +41,23 @@ class TestCycles(object):
     def test_for_in_tuple(self):
         code = "for i in (1, 2, 5): print(i)"
         assert compare_outputs(code)
+
+    def test_for_i_sum(self):
+        code = "sum_= 0\nfor i in range(5):\n\tprint(i)\n\tsum_ += i\nprint(sum_, i)"
+        assert compare_outputs(code)
+
+    def test_double_for(self):
+        code = "for i in range(8):\n\tfor j in range(9):\n\t\tprint(i * j)"
+        assert compare_outputs(code)
+
+    def test_while(self):
+        code = "i = 0\nwhile i < 10:\n\tprint(i)\n\ti += 1"
+        assert compare_outputs(code)
+
+    def test_while_break(self):
+        code = "i = 0\nwhile True:\n\tprint(i)\n\ti += 1\n\tif i >= 5: break"
+        assert compare_outputs(code)
+
+    def test_for_continue(self):
+        code = "for i in range(10):\n\tif i % 2 == 1: print(i)\n\telse: print(i % 2)"
+        assert compare_outputs(code)
